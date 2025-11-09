564f6d15e64afe639194edbe85f42f7eee5d00ac
Prince Jaiswal <princejaiswal0920@gmail.com>
2025-11-09 12:44:53 +0530

    Updated UI styling and improved form design

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,16 +1,23 @@
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
+
 const Header = () => {
   return (
-    <Navbar bg="primary" variant="dark">
-      <Container>
-        <Navbar.Brand to="/"><strong>Employee Management System</strong></Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/" className="nav-link">Employees</Nav.Link>
-          <Nav.Link as={Link} to="/employee" className="nav-link">Post Employee</Nav.Link>
+    <Navbar expand="lg" className="custom-navbar">
+      <Container className="navbar-container">
+        <Navbar.Brand as={Link} to="/" className="brand">
+          <strong>Employee Management System</strong>
+        </Navbar.Brand>
+        <Nav className="ms-auto nav-links">
+          <Nav.Link as={Link} to="/" className="nav-btn">
+            Employees
+          </Nav.Link>
+          <Nav.Link as={Link} to="/employee" className="nav-btn special">
+            Post Employee
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
